742ae1cf840feaec7fe723ad12f65c3b1844a9ca
vinaykumar860964 <vinaykumar860964@gmail.com>
2023-04-19 23:17:44 +0530

    Join method Added in multhreading

---
+++ multithreading/src/com/ADVindiancodervinaykumar/multithreading/Join_method_in_multithreading.java
@@ -0,0 +1,41 @@
+package com.ADVindiancodervinaykumar.multithreading;
+class demo extends Thread{
+	
+	public void run() {
+		try {
+			
+			for(int i=0;i<=10;i++) {
+				System.out.println("Under thread");
+				Thread.sleep(2000);
+			}
+			
+		}
+		catch(Exception e) {
+			System.out.println("Some Error");
+		}
+	}
+}
+
+class create extends Thread{
+	
+	public void run() {
+		
+		System.out.println("Just Checking");
+	}
+}
+public class Join_method_in_multithreading {
+
+	public static void main(String[] args)  throws Exception{
+		
+		System.out.println("main thread");
+		
+		demo d=new demo();
+		d.start();
+		d.join();
+		
+		create c=new create();
+		c.start();
+
+	}
+
+}
